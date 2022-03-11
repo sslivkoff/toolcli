4384c2b87bb458306290a7ffe4b1606bcd7a8da4
sslivkoff <stormslivkoff@gmail.com>
2022-03-10 18:31:37 -0800

    refine options output in help messages

--- toolcli/command_utils/help_utils/subcommand_help.py
+++ toolcli/command_utils/help_utils/subcommand_help.py
@@ -14,7 +14,11 @@ def print_subcommand_usage(parse_spec: toolcli.ParseSpec) -> None:
     for arg_spec in command_spec.get('args', []):
         name = arg_spec['name']
         if isinstance(name, str) and not name.startswith('-'):
-            required_args.append('<' + name + '>')
+            metavar = name.upper().replace('-', '_')
+            if arg_spec.get('nargs') == '?':
+                metavar = '[' + metavar + ']'
+            required_args.append(metavar)
+
         elif arg_spec.get('required'):
             if isinstance(name, str):
                 flag = name
@@ -25,9 +29,7 @@ def print_subcommand_usage(parse_spec: toolcli.ParseSpec) -> None:
                         break
                 else:
                     flag = name[0]
-            required_args.append(
-                flag + ' <' + parsing.get_arg_name(arg_spec) + '>'
-            )
+            required_args.append(flag + ' ' + get_arg_metavar(arg_spec))
 
     usage_str = config['base_command']
     if command_sequence is not None:
@@ -36,6 +38,17 @@ def print_subcommand_usage(parse_spec: toolcli.ParseSpec) -> None:
     usage_str += ' [options]'
 
     print('usage:', usage_str)
+
+
+def get_arg_metavar(arg_spec: toolcli.ArgSpec) -> str:
+    if 'metavar' in arg_spec:
+        metavar = arg_spec['metavar']
+    else:
+        metavar = parsing.get_arg_name(arg_spec).upper()
+
+    metavar = metavar.replace('-', '_')
+
+    return metavar
 
 
 def print_subcommand_help(parse_spec: toolcli.ParseSpec) -> None:
@@ -53,24 +66,32 @@ def print_subcommand_help(parse_spec: toolcli.ParseSpec) -> None:
     arg_names: list[str] = []
     arg_helps: list[str] = []
     for arg_spec in command_spec['args']:
+
+        # skip internal args
         if arg_spec.get('internal'):
             continue
+
+        # arg name
         name = arg_spec['name']
         if not isinstance(name, str):
             name = ', '.join(name)
         if isinstance(name, str) and not name.startswith('-'):
-            name = '<' + name + '>'
+            name = name.upper().replace('-', '_')
+        if name.startswith('-') and arg_spec.get('action') is None:
+            name += ' ' + get_arg_metavar(arg_spec)
         arg_names.append(name)
+
+        # arg help
         arg_help = arg_spec.get('help')
         if arg_help is None:
             arg_help = ''
         arg_helps.append(arg_help)
+
     if len(arg_names) > 0:
         max_name_len = max(len(name) for name in arg_names)
-        arg_names = [name.rjust(max_name_len) for name in arg_names]
+        arg_names = [name.ljust(max_name_len) for name in arg_names]
         print()
         print('arguments:')
-        print()
         for a in range(len(arg_names)):
             print('    ' + arg_names[a] + '    ' + arg_helps[a])
 
