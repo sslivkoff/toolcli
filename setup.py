f0e21360869191a10aff4aa1c9676f88c2edb663
sslivkoff <stormslivkoff@gmail.com>
2022-03-21 09:23:29 -0700

    bump to 0.3.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='toolcli',
-    version='0.3.5',
+    version='0.3.6',
     packages=setuptools.find_packages(),
     install_requires=[
         'ipdb',
