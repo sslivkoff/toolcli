b3f718bdf4777334fecb14be9032fc4057bd50ef
sslivkoff <stormslivkoff@gmail.com>
2022-03-21 10:04:00 -0700

    add special help message to cd subcommand

--- toolcli/command_utils/standard_subcommands/cd_command.py
+++ toolcli/command_utils/standard_subcommands/cd_command.py
@@ -14,11 +14,37 @@ def get_command_spec() -> toolcli.CommandSpec:
     }
 
 
+cd_snippet_template = """function {program_name} {
+    local tempfile="$(mktemp -t tmp.XXXXXX)"
+    command {program_name} "$@" --new_dir_tempfile "$tempfile"
+    if [[ -s "$tempfile" ]]; then
+        cd "$(cat "$tempfile")"
+    fi
+    rm -f "$tempfile" 2>/dev/null
+}"""
+
+
 def cd_command(
     dirname: str,
     new_dir_tempfile: str,
     parse_spec: toolcli.ParseSpec,
 ) -> None:
+
+    if new_dir_tempfile is None:
+        print('using the cd subcommand requires special configuration')
+        print()
+        print(
+            'add the following snippet to your shell config (e.g. ~/.profile):'
+        )
+        default_name = '<PROGRAM_NAME>'
+        program_name = parse_spec.get('config', {}).get('base_command', default_name)
+        cd_snippet = cd_snippet_template.replace('{program_name}', program_name)
+        print()
+        print(cd_snippet)
+        if program_name == default_name:
+            print()
+            print('where', default_name, 'is the name of the root command')
+        return
 
     # get path
     getter = parse_spec['config'].get('cd_dir_getter')
