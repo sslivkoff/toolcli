5d456ba8bfe2dce098b86bdde5c4057f6f660204
sslivkoff <stormslivkoff@gmail.com>
2022-04-11 12:55:30 -0700

    add help messages to "record help" parameters

--- toolcli/command_utils/standard_subcommands/record_help_command.py
+++ toolcli/command_utils/standard_subcommands/record_help_command.py
@@ -13,10 +13,31 @@ def get_command_spec():
         'f': record_help_command,
         'help': 'record help output to an html or svg file',
         'args': [
-            {'name': 'subcommand', 'nargs': '*'},
-            {'name': '--path'},
-            {'name': '--overwrite', 'action': 'store_true'},
-            {'name': '--all', 'dest': 'record_all', 'action': 'store_true'},
+            {
+                'name': 'subcommand',
+                'nargs': '*',
+                'help': 'subcommand to record help of',
+            },
+            {
+                'name': '--path',
+                'help': 'output path to html or svg file',
+            },
+            {
+                'name': '--overwrite',
+                'help': 'whether to overwrite files if they already exist',
+                'action': 'store_true',
+            },
+            {
+                'name': '--all',
+                'help': 'record help for all subcommands',
+                'dest': 'record_all',
+                'action': 'store_true',
+            },
+            {
+                'name': '--include-hidden',
+                'help': 'if using --all, include hidden subcommands',
+                'action': 'store_true',
+            },
         ],
         'special': {
             'include_parse_spec': True,
@@ -24,10 +45,19 @@ def get_command_spec():
     }
 
 
-def record_help_command(subcommand, path, overwrite, parse_spec, record_all=False):
+def record_help_command(
+    subcommand,
+    path,
+    overwrite,
+    parse_spec,
+    record_all=False,
+    include_hidden=False,
+):
 
     if record_all:
-        record_all_help_commands(path, parse_spec, overwrite)
+        record_all_help_commands(
+            path, parse_spec, overwrite, include_hidden=include_hidden
+        )
         return
 
     if path is None:
@@ -79,14 +109,19 @@ def get_default_subcommand_path(subcommand):
         return '_'.join(subcommand) + '__help.html'
 
 
-def record_all_help_commands(path, parse_spec, overwrite):
+def record_all_help_commands(path, parse_spec, overwrite, include_hidden):
 
     if path is not None:
         raise NotImplementedError('cannot specify path when recording all')
     # if path is not None and not os.path.isdir(path):
     #     raise Exception('must specify an existing dir when recording all')
 
-    for subcommand in parse_spec['command_index']:
+    for subcommand, command_spec_ref in parse_spec['command_index'].items():
+        if not include_hidden:
+            command_spec = parsing.resolve_command_spec(command_spec_ref)
+            if command_spec.get('special', {}).get('hidden'):
+                continue
+
         record_help_command(
             subcommand=subcommand,
             overwrite=overwrite,
