99c81ecc6b58989fd2275373f7ee315725340426
sslivkoff <stormslivkoff@gmail.com>
2022-07-22 14:31:29 -0700

    add default values to terminal sizing functions for when values cannot be obtained (e.g. headless mode)

--- toolcli/terminal_utils.py
+++ toolcli/terminal_utils.py
@@ -1,18 +1,29 @@
 from __future__ import annotations
 
 
-def get_n_terminal_cols() -> int:
+def get_n_terminal_cols(*, default: int | None = 80) -> int:
     import subprocess
 
     # TODO: platform independent method
-    output = subprocess.check_output('tput cols', shell=True)
-    return int(output)
+    try:
+        output = subprocess.check_output('tput cols', shell=True)
+        return int(output)
+    except Exception as e:
+        if default is not None:
+            return default
+        else:
+            raise e
 
 
-def get_n_terminal_rows() -> int:
+def get_n_terminal_rows(*, default: int | None = 80) -> int:
     import subprocess
 
     # TODO: platform independent method
-    output = subprocess.check_output('tput lines', shell=True)
-    return int(output)
-
+    try:
+        output = subprocess.check_output('tput lines', shell=True)
+        return int(output)
+    except Exception as e:
+        if default is not None:
+            return default
+        else:
+            raise e
